7251950be0e467b7c528e55a98b917965ce3ea44
johnpantini <johnpantini@github.com>
2022-01-27 21:49:15 +0300

    Allow multiple extensions without conflicts

--- desktop/leafygreen/pages/psina.js
+++ desktop/leafygreen/pages/psina.js
@@ -4,15 +4,15 @@ import { css } from '[%#ctx.ppp.rootUrl%]/shared/element/styles/css.js';
 
 export const psinaPageTemplate = (context, definition) => html`
   <template>
-    <${'ppp-page-header'}>Psina (work in progress, not ready yet!)</ppp-page-header>
+    <${'ppp-page-header'}>Psina (work in progress, not ready yet)</ppp-page-header>
   </template>
 `;
 
 export const psinaPageStyles = (context, definition) => css``;
 
 // noinspection JSUnusedGlobalSymbols
-export const psinaPage = PsinaPage.compose({
-  baseName: 'psina-page',
+export default PsinaPage.compose({
+  baseName: 'psina-[%#payload.extension._id%]-page',
   template: psinaPageTemplate,
   styles: psinaPageStyles
 });
